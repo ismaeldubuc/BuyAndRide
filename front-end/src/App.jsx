48f40a13d0e27df4ac95c332c6aeacf2fb546cf4
BenjaminLeclerc1 <b.leclercbenjamin@gmail.com>
2025-02-19 11:41:29 +0100

    refacto

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,4 @@
 import "./App.css";
-import { FiChevronDown, FiTrash, FiShare, FiPlusSquare } from "react-icons/fi";
-import { SiBmw } from "react-icons/si";
-import { SiToyota } from "react-icons/si";
-import { SiDacia } from "react-icons/si";
-import { SiMitsubishi } from "react-icons/si";
-
-import { motion } from "framer-motion";
-import { useState } from "react";
-import Login from "./components/login";
-import React from 'react';
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import VehicleDetails from './components/VehicleDetails';
-import Layout from './components/layout';
 import MesRoutes from './MesRoutes';
 
 function App() {
